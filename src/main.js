1e430475527a38bdfc176cc8ace85c0f7e55cf94
sunwangda <2451203734@qq.com>
2020-03-24 00:47:13 +0800

    登录页面的实现

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 导入全局样式表
+import './assets/css/global.css'
+import './assets/fonts/iconfont.css'
+
+import axios from 'axios'
+axios.defaults.baseURL = 'localhost:8082/hf-user'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
